71f7421dce67bb5be3ff5e37a130099903429cbc
Max Haviv <me03haviv@gmail.com>
2024-08-16 13:03:53 -0400

    Finished Email Verification logic
    - toast is causing issues.
    - either redirect from api route or figure out what is wrong with react toast.

--- src/app/api/users/verifyEmail/route.ts
+++ src/app/api/users/verifyEmail/route.ts
@@ -7,13 +7,35 @@ import { createSessionClient } from "@/lib/server/appwrite";
 export async function POST(request: NextRequest) {
   try {
     const { account } = await createSessionClient()
-    // const user = await account.get()
 
     const result = await account.createVerification(
       'http://localhost:3000/profile/verifyEmail' // url to redirect user to in confirmation email
     )
 
     return NextResponse.json({message: "Successfully sent verification email.", result: result, success: true})
+  }
+  catch (error: any) {
+    return NextResponse.json({message: "Failed to create email verification.", error: error}, {status: 400})
+  }
+}
+
+// take params from email verification and try to verify email
+// 
+// this function gets run when the user routes to the verifyemail page with 
+// the params passed through search params
+export async function PUT(request: NextRequest) {
+  const response = await request.json()
+  const {userId, secret} = response
+
+  try {
+    const { account } = await createSessionClient()
+
+    const result = await account.updateVerification(
+      userId,
+      secret
+    )
+
+    return NextResponse.json({message: "Verified email successfully.", result: result, success: true})
   }
   catch (error: any) {
     return NextResponse.json({message: "Failed to verify email.", error: error}, {status: 400})
--- src/app/profile/verifyEmail/page.tsx
+++ src/app/profile/verifyEmail/page.tsx
@@ -1,12 +1,67 @@
+'use client'
+import { useSearchParams } from "next/navigation"
+import { useEffect, useState } from "react"
+import axios from "axios"
+import toast from "react-hot-toast"
+import { useRouter } from "next/navigation"
 
+type params = {
+  userId: string | null,
+  secret: string | null,
+  expire: string | null
+}
 
 export default function verifyEmailPage() {
+  const router = useRouter()
+  // const [verificationParams, setVerificationParams] = useState<params>({
+  //   userId: "",
+  //   secret: "",
+  //   expire: "",
+  // })
+
+  // verification paramaters passed through the url
+  const params = useSearchParams()
+
+  // const getVerificationParams = () => {
+  //   setVerificationParams({
+  //     userId: params.get('userId'),
+  //     secret: params.get('secret'),
+  //     expire: params.get('expire')
+  //   })
+  // }
+
+  const getVerificationParams = () => {
+    return {
+      userId: params.get('userId'),
+      secret: params.get('secret'),
+      expire: params.get('expire')
+    }
+  }
+
+  const verifyEmail = async (verifyParams: params) => {
+    try {
+      const response = await axios.put('/api/users/verifyEmail', verifyParams)
+      // toast.success("Successfully Verified Email")
+      router.push('/profile')
+    }
+    catch (error: any) {
+      // toast.error(error.response.data)
+    }
+  }
+
+  // get paramaters on load of the page
+  // then call verification api route
+  useEffect(() => {
+    const params = getVerificationParams()
+
+    verifyEmail(params)
+  }, [])
 
   return (
     <div className="min-h-screen flex flex-col justify-center bg-background
     absolute inset-0 -z-10 h-full w-full items-center px-5 py-24
     bg-[radial-gradient(circle,_var(--tw-gradient-stops))] from-default-50 from-10% to-danger-200 to-65% text-default-800">
-      Page
+      <p>hello</p>
     </div>
   )
 }
